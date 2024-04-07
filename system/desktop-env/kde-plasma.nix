acf0ffe9a34aba05d5fab158e39b52e56dab01f4
Maximilian Jakob Maag <maximilian.maag@outlook.de>
2024-04-08 00:52:12 +0200

    add a11 extension to gnome

---
+++ system/desktop-env/kde-plasma.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+    services.xserver.enable = true;
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
+
+    environment.systemPackages = with pkgs;
+    [
+        gnomeExtensions.true-color-window-invert
+    ]
+}
