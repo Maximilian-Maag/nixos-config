cc361cf64f1a513277a7fc9d13bbb4f5ef41065f
Maximilian-Maag <maximilian.maag@outlook.de>
2024-04-10 17:10:50 +0200

    fix hm mngr warnings

--- system/user/mmaag.nix
+++ system/user/mmaag.nix
@@ -1,29 +1,27 @@
 # This file is a sample for user configuration
 { config, pkgs, ...}:
-
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz";
+in
 {
     # include home manager to configer user settings
-    imports = 
-    [
-        ./home-manager.nix
+    imports = [
+        (import "${home-manager}/nixos")
     ];
     
-    # enable home manager
-    home-manager.users.mmaag = {
-        /* The home.stateVersion option does not have a default and must be set */
-        home.stateVersion = "23.11";
-        programs.git = {
-            enable = true;
-            userName  = "Maximilian-Maag";
-            userEmail = "maximilian.maag@outlook.de";
-        };
-        /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
-    };
-
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.mmaag = {
         isNormalUser = true;
         description = "Maximilain Jakob Maag";
         extraGroups = [ "networkmanager" "wheel" ];
+    };
+
+    home-manager.users.mmaag = {
+        programs.git = {
+            enable = true;
+            userName  = "Maximilian-Maag";
+            userEmail = "maximilian.maag@outlook.de";
+        };
+        home.stateVersion = "23.11";
     };
 }
