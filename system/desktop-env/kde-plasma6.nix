c8535d929ee803a10089282be1879d555dea0ffc
Maximilian Jakob Maag <maximilian.maag@outlook.de>
2024-07-15 22:18:03 +0200

    update

--- system/desktop-env/kde-plasma6.nix
+++ system/desktop-env/kde-plasma6.nix
@@ -3,15 +3,14 @@
 {
     # this config is ment for nixos 24.05
     services.xserver.enable = true;
-    services.xserver.desktopManager.plasma6.enable = true;
+    services.desktopManager.plasma6.enable = true;
     services.xserver.displayManager.lightdm.enable = true;
-    #services.xserver.displayManager.lightdm.background = "somepath";
     services.xserver.windowManager.stumpwm.enable = true;
-    services.xserver.xkbOptions = "ctrl:nocaps";
-    services.xserver.libinput.enable = false; # Enable touchpad support
+    services.xserver.xkb.options = "ctrl:nocaps";
+    services.libinput.enable = false; # Enable touchpad support
 
     # specific clients for this desktop environment
-    environment.systemPackages = with pkgs [
-    	nextcloud-client
+    environment.systemPackages = with pkgs; [
+        nextcloud-client
     ];
 }
