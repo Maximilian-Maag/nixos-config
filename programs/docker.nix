3614ba3e4b5345d894b56fd810e7994dbdb5581f
Maximilian Jakob Maag <maximilian.maag@outlook.de>
2024-10-05 22:28:16 +0200

    add docker-compose to docker config

--- programs/docker.nix
+++ programs/docker.nix
@@ -3,9 +3,15 @@
 {
   virtualisation.docker.enable = true;
   users.users.mmaag.extraGroups = [ "docker" ];
-  users.extraGroups.docker.members = [ "mmaag" ];
+  users.extraGroups.docker.members = [ "mmaag" ]; #use your username
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
   };
+
+  # also adding docker-compose
+  environment.systemPackages  = with pkgs;
+  [
+    docker-compose
+  ];
 }
