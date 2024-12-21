a85d1d38b0f8f157db062afa84dfe734ba6a022b
Maximilian Jakob Maag <maximilian.maag@outlook.de>
2024-12-21 17:16:00 +0100

    save

--- programs/ollama-amd.nix
+++ programs/ollama-amd.nix
@@ -4,9 +4,6 @@
     services.ollama = {
         enable = true;
         acceleration = "rocm";
-        environmentVariables = {
-            HCC_AMDGPU_TARGET = "gfx1101"; # used to be necessary, but doesn't seem to anymore
-        };
-        rocmOverrideGfx = "1101"; # this value depends on your machine
+        rocmOverrideGfx = "11.0.1";
     };
 }
