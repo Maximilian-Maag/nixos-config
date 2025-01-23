be5212c52dd42fc45118ee1959bba0041b270e65
Maximilian Jakob Maag <maximilian.maag@outlook.de>
2025-01-23 15:10:13 +0100

    save

--- projects/obs-screencast.nix
+++ projects/obs-screencast.nix
@@ -25,6 +25,11 @@
         "v4l2loopback"
     ];
 
+    boot.extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
+    security.polkit.enable = true;
+
     hardware.graphics = {
         enable = true;
         extraPackages = with pkgs; [
