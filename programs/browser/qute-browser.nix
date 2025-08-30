a2a3b48a92c4c2dc5ca678d1e68f8efba2f69ced
Maximilian Jakob Maag <maximilian.maag@outlook.de>
2025-08-30 03:38:15 +0200

    adding qute browser less finger printing

---
+++ programs/browser/qute-browser.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+let
+  adblockLists = [
+    "https://www.i-dont-care-about-cookies.eu/abp/"
+    "https://easylist.to/easylist/easylist.txt"
+    "https://easylist.to/easylist/easyprivacy.txt"
+    "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt"
+    "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt"
+    "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt"
+    "https://filters.adtidy.org/extension/ublock/filters/224.txt"
+    "https://www.i-dont-care-about-cookies.eu/abp/"
+    "https://easylist-downloads.adblockplus.org/fanboy-annoyance.txt"
+  ];
+in
+{
+  environment.systemPackages = with pkgs; [
+    qutebrowser
+    mpv # vid player that will open video in detached window
+    yt-dlp # used to stream vid from yt server
+  ];
+
+  # generate a script for add free youtube vids with home manager
+  home-manager.users.mmaag = {
+    home.file.".config/qutebrowser/youtube-to-vlc.sh" = {
+      text = ''
+        #!/usr/bin/env bash
+        url="$1"
+        mpv --ytdl-format="bestvideo[height<=1080]+bestaudio/best" "$url"
+      '';
+      executable = true;
+    };
+
+    programs.qutebrowser = {
+      enable = true;
+      settings = {
+        content.blocking.enabled = true;
+        content.blocking.method = "adblock";
+        content.blocking.adblock.lists = adblockLists;
+        fonts.default_size = "30pt";
+        content.cookies.accept = "no-3rdparty";  # Fix here
+        content.javascript.enabled = true;
+        content.cookies.store = false;
+        content.ssl_strict = true;
+        content.canvas_reading = false;
+        content.webrtc_ip_handling_policy = "disable_non_proxied_udp";
+
+        # User agent spoofing
+        content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0";
+      };
+
+      keyBindings.normal = {
+        "M" = "hint links spawn --detach /home/mmaag/.config/qutebrowser/youtube-to-vlc.sh {hint-url}";
+        "cr" = "jseval (function () { var i, elements = document.querySelectorAll(\"body *\"); for (i = 0; i < elements.length; i++) { var pos = getComputedStyle(elements[i]).position; if (pos === \"fixed\" || pos === \"sticky\") { elements[i].parentNode.removeChild(elements[i]); } } })();";
+      };
+    };
+  };
+}
